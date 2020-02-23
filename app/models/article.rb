e9779af2c707caaf6ac4a251fd0b91e26ef02547
tk0358 <tk0358a@yahoo.co.jp>
2020-02-23 18:50:23 +0900

    Add favicon

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,11 +6,9 @@ class Article < ApplicationRecord
   def self.search
     url = URI.parse("http://newsapi.org/v2/everything?q=japan&sortBy=popularity&apiKey=#{Rails.application.credentials.news_api[:api_key]}")
     res = Net::HTTP.get_response(url)
-    if res
-      json = JSON.parse(res.body)
-      article = json["articles"][0]
+    json = JSON.parse(res.body)
+    article = json["articles"][0]
 
-      new(title: article["title"], content: article["content"], date: article["publishedAt"])
-    end
+    new(title: article["title"], content: article["content"], date: article["publishedAt"])
   end
 end
